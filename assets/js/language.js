0cee5d13f54cdad40e3bba42744c054cb2ad4783
Hugoow0 <hugodev04@gmail.com>
2024-01-10 14:02:29 +0100

    change language fr-en

---
+++ assets/js/language.js
@@ -0,0 +1,41 @@
+// Create a function to change
+// the hash value of the page
+
+/*function changeLanguage(lang) {
+    location.hash = lang;
+    //location.reload();
+
+    if (location.hash == "#fr") {
+        siteContent.textContent = language.es.welcome;
+    }
+    else if (location.hash == "#en") {
+        siteContent.textContent = language.en.welcome;
+    }
+}
+
+// Define the language reload anchors
+let language = {
+    es: {
+        welcome: "Bienvenue sur mon portfolio!"
+    },
+    en: {
+        welcome: "Welcome to my Portfolio!",
+
+    }
+};*/
+
+// Check if a hash value exists in the URL
+/*if (window.location.hash) {
+
+    // Set the content of the webpage
+    // depending on the hash value
+    if (window.location.hash == "#fr") {
+        siteContent.textContent =
+            language.es.welcome;
+    }
+    else if (window.location.hash == "#en") {
+        siteContent.textContent =
+            language.en.welcome;
+    }
+}*/
+
